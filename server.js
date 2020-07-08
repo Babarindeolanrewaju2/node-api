bc941787d1956e51baf8eaccdb3742825623d96b
Babarindeolanrewaju2 <babarindeolanrewaju@gmail.com>
2020-07-08 23:21:55 +0100

    nodemon installation

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ dotEnv.config();
 
 const app = express();
 
-const PORT = proccess.env.PORT || 3000;
+app.get('/', (req, res, next) => {
+    res.send('Hello from Node Server')
+})
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
