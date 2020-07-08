c33966735c0494306409b4de4861a449df7e80e9
Babarindeolanrewaju2 <babarindeolanrewaju@gmail.com>
2020-07-08 23:31:34 +0100

    middleware cors

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const dotEnv = require("dotenv");
+const cors = require('cors');
 
 dotEnv.config();
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const myMiddleware = (req, res, next) => {
     console.log('Hey Wassup');
@@ -23,6 +27,10 @@ app.listen(PORT, () => {
 
 app.use((err, req, res, next) => {
     console.error(err.stack)
-    res.status(500).send('Something broke!')
+    res.status(500).send({
+        status: 500,
+        message: err.message,
+        body: {}
+    })
     next()
 })
