705581b276510974cfcfe6e9e81c107e3ac10243
Babarindeolanrewaju2 <babarindeolanrewaju@gmail.com>
2020-07-08 23:28:11 +0100

    middleware error

--- server.js
+++ server.js
@@ -20,3 +20,9 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 })
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+    next()
+})
