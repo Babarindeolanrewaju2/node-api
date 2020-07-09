0cdc0d85263f25614428f8b7e44f89b47b7df323
Babarindeolanrewaju2 <babarindeolanrewaju@gmail.com>
2020-07-09 01:03:38 +0100

    messages fixed

--- database/models/productModel.js
+++ database/models/productModel.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        index: true       //---Index----
-    },
+    name: String,
     price: Number,
     brand: String,
 
