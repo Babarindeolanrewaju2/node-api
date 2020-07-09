6d57cf6889ee0b7542bc98b2703644cc5eabb39e
Babarindeolanrewaju2 <babarindeolanrewaju@gmail.com>
2020-07-09 01:08:50 +0100

    toObject transform

--- database/models/productModel.js
+++ database/models/productModel.js
@@ -6,7 +6,15 @@ const productSchema = new mongoose.Schema({
     brand: String,
 
 }, {
-    timestamps: true
+    timestamps: true,
+    toObject: {
+        transform: function (doc, ret, options) {
+            ret.id = ret.id;
+            delete ret._id;
+            delete ret.__v;
+            return ret;
+        }
+    }
 })
 
 module.exports = mongoose.model('Product', productSchema);
--- service/productService.js
+++ service/productService.js
@@ -8,7 +8,8 @@ module.exports.createProduct = async (serviceData) => {
     // })
     try {
         let product = new Product({ ...serviceData });
-        return await product.save();
+        let result = await product.save();
+        return result.toObject();
     } catch (error) {
         console.log('Something went wrong: Service: createProduct', error)
         throw new Error(error);
