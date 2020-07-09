fa7653b3a7343ac51238180b6ad81ccfd6956ece
Babarindeolanrewaju2 <babarindeolanrewaju@gmail.com>
2020-07-09 15:24:33 +0100

    middleware added

--- database/models/productModel.js
+++ database/models/productModel.js
@@ -9,13 +9,12 @@ const productSchema = new mongoose.Schema({
         required: true
     },
     price: Number,
-    brand: String,
-
+    brand: String
 }, {
     timestamps: true,
     toObject: {
         transform: function (doc, ret, options) {
-            ret.id = ret.id;
+            ret.id = ret._id;
             delete ret._id;
             delete ret.__v;
             return ret;
