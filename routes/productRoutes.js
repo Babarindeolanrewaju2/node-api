37f3b6b3ee7e312333966cd012b38b3e4575190f
Babarindeolanrewaju2 <babarindeolanrewaju@gmail.com>
2020-07-12 21:06:36 +0100

    added the update and delete functions

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,20 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const productController = require('../controller/productController');
-const joiSchemaValidation = require('../middleware/joiSchemaValidation');
-const productSchema = require('../apiSchema/productSchema');
+const productController = require("../controller/productController");
+const joiSchemaValidation = require("../middleware/joiSchemaValidation");
+const productSchema = require("../apiSchema/productSchema");
 
-router.post('/',
-    joiSchemaValidation.validateBody(productSchema.createProductSchema),
-    productController.createProduct)
-
-router.get('/:id',
-    productController.getProductById
+router.post(
+  "/",
+  joiSchemaValidation.validateBody(productSchema.createProductSchema),
+  productController.createProduct
 );
 
+router.get("/:id", productController.getProductById);
 
-router.get('/',
-    joiSchemaValidation.validateQueryParams(productSchema.getAllProductSchema),
-    productController.getAllProducts)
+router.get(
+  "/",
+  joiSchemaValidation.validateQueryParams(productSchema.getAllProductSchema),
+  productController.getAllProducts
+);
+
+router.put(
+  "/:id",
+  joiSchemaValidation.validateBody(productSchema.updateProductSchema),
+  productController.updateProduct
+);
+
+router.delete("/:id", productController.deleteProduct);
 
 module.exports = router;
--- service/productService.js
+++ service/productService.js
@@ -1,44 +1,75 @@
-const Product = require('../database/models/productModel')
-const { formatMongoData, checkObjectId } = require('../helper/dbHelper');
-const constants = require('../constants');
+const Product = require("../database/models/productModel");
+const { formatMongoData, checkObjectId } = require("../helper/dbHelper");
+const constants = require("../constants");
 
 module.exports.createProduct = async (serviceData) => {
-    // let product = new Product({
-    //     name: serviceData.name,
-    //     price: serviceData.price,
-    //     brand: serviceData.brand
-    // })
-    try {
-        let product = new Product({ ...serviceData });
-        let result = await product.save();
-        return result.toObject();
-    } catch (error) {
-        console.log('Something went wrong: Service: createProduct', error)
-        throw new Error(error);
-    }
-}
+  // let product = new Product({
+  //     name: serviceData.name,
+  //     price: serviceData.price,
+  //     brand: serviceData.brand
+  // })
+  try {
+    let product = new Product({ ...serviceData });
+    let result = await product.save();
+    return result.toObject();
+  } catch (error) {
+    console.log("Something went wrong: Service: createProduct", error);
+    throw new Error(error);
+  }
+};
 
 module.exports.getAllProducts = async ({ skip = 0, limit = 10 }) => {
-    try {
-        let products = await Product.find({}).skip(parseInt(skip)).limit(parseInt(limit));
-        return formatMongoData(products);
-    } catch (error) {
-        console.log('Something went wrong: Service: getAllProducts', error);
-        throw new Error(error);
-    }
-}
-
+  try {
+    let products = await Product.find({})
+      .skip(parseInt(skip))
+      .limit(parseInt(limit));
+    return formatMongoData(products);
+  } catch (error) {
+    console.log("Something went wrong: Service: getAllProducts", error);
+    throw new Error(error);
+  }
+};
 
 module.exports.getProductById = async ({ id }) => {
-    try {
-        checkObjectId(id);
-        let product = await Product.findById(id);
-        if (!product) {
-            throw new Error(constants.productMessage.PRODUCT_NOT_FOUND);
-        }
-        return formatMongoData(product);
-    } catch (error) {
-        console.log('Something went wrong: Service: getProductById', error);
-        throw new Error(error);
+  try {
+    checkObjectId(id);
+    let product = await Product.findById(id);
+    if (!product) {
+      throw new Error(constants.productMessage.PRODUCT_NOT_FOUND);
     }
-}
+    return formatMongoData(product);
+  } catch (error) {
+    console.log("Something went wrong: Service: getProductById", error);
+    throw new Error(error);
+  }
+};
+
+module.exports.updateProduct = async ({ id, updateInfo }) => {
+  try {
+    checkObjectId(id);
+    let product = await Product.findOneAndUpdate({ _id: id }, updateInfo, {
+      new: true,
+    });
+    if (!product) {
+      throw new Error(constants.productMessage.PRODUCT_NOT_FOUND);
+    }
+    return formatMongoData(product);
+  } catch (error) {
+    console.log("Something went wrong: Service: updateProduct", error);
+    throw new Error(error);
+  }
+};
+
+module.exports.deleteProduct = async ({ id }) => {
+  try {
+    checkObjectId(id);
+    let product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      throw new Error(constants.productMessage.PRODUCT_NOT_FOUND);
+    }
+    return formatMongoData(product);
+  } catch (error) {
+    console.log("Something went wrong: Service: deleteProduct", error);
+    throw new Error(error);
+  }
+};
