4ca75130cebab93e2a86cdac0888aae736fa6488
Babarindeolanrewaju2 <babarindeolanrewaju@gmail.com>
2020-07-12 21:36:06 +0100

    added the user login and signup

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,27 +3,39 @@ const router = express.Router();
 const productController = require("../controller/productController");
 const joiSchemaValidation = require("../middleware/joiSchemaValidation");
 const productSchema = require("../apiSchema/productSchema");
+const tokenValidation = require("../middleware/tokenValidation");
 
 router.post(
   "/",
+  tokenValidation.validateToken,
   joiSchemaValidation.validateBody(productSchema.createProductSchema),
   productController.createProduct
 );
 
-router.get("/:id", productController.getProductById);
-
 router.get(
-  "/",
-  joiSchemaValidation.validateQueryParams(productSchema.getAllProductSchema),
-  productController.getAllProducts
+  "/:id",
+  tokenValidation.validateToken,
+  productController.getProductById
 );
 
 router.put(
   "/:id",
+  tokenValidation.validateToken,
   joiSchemaValidation.validateBody(productSchema.updateProductSchema),
   productController.updateProduct
 );
 
-router.delete("/:id", productController.deleteProduct);
+router.get(
+  "/",
+  tokenValidation.validateToken,
+  joiSchemaValidation.validateQueryParams(productSchema.getAllProductSchema),
+  productController.getAllProducts
+);
+
+router.delete(
+  "/:id",
+  tokenValidation.validateToken,
+  productController.deleteProduct
+);
 
 module.exports = router;
